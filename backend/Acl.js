961d4a5a9866c66de54edf389e1891280f599e1e
Svenpaj <114157351+Svenpaj@users.noreply.github.com>
2023-04-12 14:20:25 +0200

    small bugg fixes

--- backend/Acl.js
+++ backend/Acl.js
@@ -6,12 +6,30 @@ module.exports = class Acl {
   // req.sesssion.user -> logged in user if any
 
   static checkRoute(req, table, method, isTable, isView) {
+    let role = req.session.user ? (req.session.user.userRole || ' logged in') : ' not logged in';
+    
+    
     console.log([
-      'user: ' + JSON.stringify(req.session.user, '', ' '),
+      'role: ' + role,
       'url: ' + req.url,
       'table: ' + table,
       'method: ' + method,
     ].join('/n'));
+
+    return true;
+
+    if (role === ' not logged in' && table === 'users' && method !== 'GET') {
+      return true;
+    }
+
+    if (role === ' not logged in' && method !== 'GET') {
+      return false;
+    }
+
+    if (role !== 'admin' && table === 'users') {
+      return false;
+    }
+
     return true;
   }
 
