d2b07e86257c913e041d8743be403aad28fb6a5c
Svenpaj <114157351+Svenpaj@users.noreply.github.com>
2023-04-12 13:51:20 +0200

    new changes, trigger, added user role and tests

--- backend/Acl.js
+++ backend/Acl.js
@@ -6,6 +6,12 @@ module.exports = class Acl {
   // req.sesssion.user -> logged in user if any
 
   static checkRoute(req, table, method, isTable, isView) {
+    console.log([
+      'user: ' + JSON.stringify(req.session.user, '', ' '),
+      'url: ' + req.url,
+      'table: ' + table,
+      'method: ' + method,
+    ].join('/n'));
     return true;
   }
 
